42045efb4579efabae93c435024b80d03ea6cf5c
jackhsu <contactjackhsu@gmail.com>
2022-11-02 16:58:59 +0000

    set up server

--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,47 @@
-import express from 'express'
-import auth  from '../middleware/authorization.js'
-import multer  from "multer"
+import express from "express";
+import auth from "../middleware/auth.js";
+import multer from "multer";
 
-import { 
-    createTweet,
-    followManager,
-    getHome,
-    getUser,
-    getUserTweets,
-    login, 
-    logintoken, 
-    register,
-    updateUser,
-    uploadImages
-} from '../controllers/user.controller.js'
+import {
+  createTweet,
+  followManager,
+  getHome,
+  getUser,
+  getUserTweets,
+  login,
+  logintoken,
+  register,
+  updateUser,
+  uploadImages,
+} from "../controllers/user.controller.js";
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+});
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res) => res.send("just dev."))
+router.get("/", (req, res) => res.send("just dev."));
 
-router.get("/home", getHome)
+router.get("/home", getHome);
 
-router.get("/user", getUser)
+router.get("/user", getUser);
 
-router.get("/userTweets", getUserTweets)
+router.get("/userTweets", getUserTweets);
 
-router.post("/register", register)
+router.post("/register", register);
 
-router.post("/login", login)
+router.post("/login", login);
 
-router.post("/auth", logintoken)
+router.post("/auth", logintoken);
 
-router.post("/tweet", auth, createTweet)
+router.post("/tweet", auth, createTweet);
 
-router.post("/upload", auth, upload.array('files'), uploadImages)
+router.post("/upload", auth, upload.array("files"), uploadImages);
 
-router.post("/update", auth, updateUser)
+router.post("/update", auth, updateUser);
 
-router.post("/follow", auth, followManager)
+router.post("/follow", auth, followManager);
 
-export default router
+export default router;
